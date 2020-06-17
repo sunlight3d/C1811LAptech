aee736c72f1b2dd90a0a656e3d6263fa26dde5fe
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-06-17 20:19:10 +0700

    update

---
+++ 17-06-2020/15-06-2020/Validation/Validation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _15_06_2020.Validation
+{
+    public class Validation
+    {
+        public static Boolean IsValidTelephone(string telephone)
+        {
+            Regex regex = new Regex(@"^0[\d]{5,}$");
+            return regex.IsMatch(telephone);
+        }
+    }
+}
