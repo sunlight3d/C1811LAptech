a027ea9284e4694852bdc43cce68f89c88d9bfeb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-07-06 21:37:54 +0700

    update

---
+++ ASPdotNetMVC/06-07-2020/bai01/bai01/Models/Exam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai01.Models
+{
+    public class Exam
+    {
+        private static int id = 0;
+        public int ExamId { get; set; }
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public int Mark { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Subject Subject { get; set; }
+        public Exam() {
+            id++;
+            ExamId = id;
+        }
+    }
+}
---
+++ ASPdotNetMVC/06-07-2020/bai01/bai01/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai01.Models
+{
+    public class Student
+    {
+        private static int id = 0;
+        
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime StudentDOB { get; set; }
+        public string StudentClass { get; set; }
+        public ICollection<Exam> Exams { get; set; }
+        public Student() {
+            this.Exams = new HashSet<Exam>();
+            id++;
+            StudentId = id;
+        }
+
+    }
+}
