2b68e4f9185be2f8a8801e66fdebda957abf13fd
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-08-03 22:22:46 +0700

    update

---
+++ ASPdotNetMVC/31-07-2020/WAD_Hoang/WAD_Hoang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WAD_Hoang.Startup))]
+namespace WAD_Hoang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
